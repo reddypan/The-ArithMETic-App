56ab4a0de17511d364f79cf6c375a5272253c37c
student <student@nw46143.local>
2019-02-14 18:34:32 -0600

    Initial Commit

---
+++ The ArithMETic App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The ArithMETic App
+//
+//  Created by student on 2/14/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
